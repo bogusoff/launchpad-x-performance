54d3d5291f8838b33e9d8281a96e39d804c1f623
Евгений Богусов <bogusoff@MacBook-Air.local>
2026-07-07 23:11:59 +0700

    v0.4.0 Toggle Clip

--- src/Launchpad_X_performance/__init__.py
+++ src/Launchpad_X_performance/__init__.py
@@ -9,6 +9,24 @@ from ableton.v2.control_surface.capabilities import (
     inport,
     outport,
 )
+from ableton.v2.control_surface.components.clip_slot import ClipSlotComponent
+
+
+_original_do_launch_clip = ClipSlotComponent._do_launch_clip
+
+
+def _toggle_do_launch_clip(self, fire_state):
+    if fire_state and self.has_clip():
+        clip = self._clip_slot.clip
+
+        if clip.is_playing:
+            clip.stop()
+            return
+
+    _original_do_launch_clip(self, fire_state)
+
+
+ClipSlotComponent._do_launch_clip = _toggle_do_launch_clip
 
 from .launchpad_x import Launchpad_X
 
