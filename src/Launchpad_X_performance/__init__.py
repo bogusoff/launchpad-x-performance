ef38bc0f09a1b46ba02d362d56262633ea50c816
Евгений Богусов <bogusoff@MacBook-Air.local>
2026-07-10 14:10:36 +0700

    v0.8.0 quantized stop indication

--- src/Launchpad_X_performance/__init__.py
+++ src/Launchpad_X_performance/__init__.py
@@ -1,3 +1,4 @@
+from ableton.v2.base import task
 from ableton.v2.control_surface.capabilities import (
     CONTROLLER_ID_KEY,
     NOTES_CC,
@@ -14,10 +15,63 @@ from ableton.v2.control_surface.components.scene import SceneComponent
 
 
 LONG_PRESS_TICKS = 10
+STOP_INDICATION_REFRESH = 0.05
 
 
 _original_do_launch_clip = ClipSlotComponent._do_launch_clip
 _original_do_launch_scene = SceneComponent._do_launch_scene
+
+
+def _start_stop_indication(self, track, clip, button):
+    previous_task = getattr(
+        self,
+        "_performance_stop_indication_task",
+        None,
+    )
+
+    if previous_task is not None:
+        previous_task.kill()
+
+    task_holder = {}
+
+    def _refresh_stop_indication():
+        indicator_task = task_holder.get("task")
+
+        same_clip = (
+            self.has_clip()
+            and self._clip_slot.clip == clip
+        )
+
+        same_button = (
+            self.launch_button.control_element == button
+        )
+
+        stop_is_pending = (
+            same_clip
+            and same_button
+            and clip.is_playing
+            and track.fired_slot_index == -2
+        )
+
+        if stop_is_pending:
+            button.set_light("Session.StopClipTriggered")
+            return
+
+        if indicator_task is not None:
+            indicator_task.kill()
+
+        self._performance_stop_indication_task = None
+        self._update_launch_button_color()
+
+    indicator_task = self._tasks.add(
+        task.loop(
+            task.wait(STOP_INDICATION_REFRESH),
+            task.run(_refresh_stop_indication),
+        )
+    )
+
+    task_holder["task"] = indicator_task
+    self._performance_stop_indication_task = indicator_task
 
 
 def _toggle_do_launch_clip(self, fire_state):
@@ -26,12 +80,18 @@ def _toggle_do_launch_clip(self, fire_state):
 
         if clip.is_playing:
             track = self._clip_slot.canonical_parent
-            track.stop_all_clips()
-
             button = self.launch_button.control_element
+
+            track.stop_all_clips()
 
             if button is not None:
                 button.set_light("Session.StopClipTriggered")
+                _start_stop_indication(
+                    self,
+                    track, 
+                    clip, 
+                    button,
+                )
 
             return
 
@@ -49,16 +109,24 @@ def _scene_restart_or_stop(self, value):
         def _mark_long_press():
             if getattr(self, "_performance_scene_pressed", False):
                 self._performance_scene_long_pressed = True
+
                 for clip_slot in self._scene.clip_slots:
                     clip_slot.stop()
 
-        self.canonical_parent.schedule_message(LONG_PRESS_TICKS, _mark_long_press)
+        self.canonical_parent.schedule_message(
+            LONG_PRESS_TICKS,
+            _mark_long_press,
+        )
 
     else:
         if getattr(self, "_performance_scene_pressed", False):
             self._performance_scene_pressed = False
 
-            if not getattr(self, "_performance_scene_long_pressed", False):
+            if not getattr(
+                self,
+                "_performance_scene_long_pressed",
+                False,
+            ):
                 _original_do_launch_scene(self, True)
                 _original_do_launch_scene(self, False)
 
